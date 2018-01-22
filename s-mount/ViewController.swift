f865c8f9cbb9cc5048359ff66f435637ba48a7c7
Dave Gumba <dave_gumba@Yahoo.com>
2018-01-22 14:47:26 -0500

    setup project with a gitignore, and with a linter

---
+++ s-mount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  s-mount
+//
+//  Created by Dave Gumba on 2018-01-22.
+//  Copyright © 2018 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
