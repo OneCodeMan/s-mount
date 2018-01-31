1838de2dee304075bb91f1635e810f9dced0db74
Dave Gumba <dave_gumba@Yahoo.com>
2018-01-30 21:46:27 -0500

    working on custom cell

---
+++ s-mount/SessionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SessionViewController.swift
+//  s-mount
+//
+//  Created by Dave Gumba on 2018-01-30.
+//  Copyright © 2018 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class SessionViewController: UIViewController {
+    
+    @IBOutlet weak var movesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        movesTableView.delegate = self
+        movesTableView.dataSource = self
+        
+    }
+
+}
+
+extension SessionViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MoveCell", for: indexPath)
+        cell.textLabel?.text = "yo"
+        return cell
+    }
+    
+    
+}
